ffde7af311512b3b7583e1b89f9325f4ac208cf9
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-03-10 15:47:02 +0300

    Add. Connection pool example

---
+++ examples/pool.lua
@@ -0,0 +1,110 @@
+local uv = require "lluv"
+local ut = require "lluv.utils"
+local pg = require "lluv.pg"
+local EventEmitter = require "EventEmitter".EventEmitter
+
+-- Pool does not track any query activity
+-- it just do round robin for multiple connection
+local PGConnectionPool = ut.class(EventEmitter) do
+local Pool = PGConnectionPool
+
+local function append(t, v)
+  t[#t + 1] = v
+  return t
+end
+
+local function remove_value(t, v)
+  for i = 1, #t do
+    if t[i] == v then
+      return table.remove(t, i)
+    end
+  end
+end
+
+local function shift_value(t)
+  local db = table.remove(t, 1)
+  append(t, db)
+  return db
+end
+
+local function super(self, m, ...)
+  if self.__base and self.__base[m] then
+    return self.__base[m](self, ...)
+  end
+  return self
+end
+
+function Pool:__init(n, cfg)
+  self = super(self, '__init')
+
+  self._active, self._waiting = {}, {}
+
+  for i = 1, n do
+    local db = pg.new(cfg)
+
+    db:on('ready', function(db)
+      self._waiting[db] = nil
+      append(self._active, db)
+      self:emit('connection::ready', db)
+    end)
+
+    db:on('close', function(db, event, err)
+      remove_value(self._active, db)
+      self._waiting[db] = true
+      self:emit('connection::close', db, err)
+    end)
+
+    append(self._active, db)
+
+    db:connect()
+  end
+
+  return self
+end
+
+function Pool:get()
+  return shift_value(self._active)
+end
+
+function Pool:close()
+  for db in pairs(self._waiting) do
+    db:close()
+  end
+
+  for _, db in ipairs(self._active) do
+    db:close()
+  end
+
+  self._active  = {}
+  self._waiting = {}
+end
+
+end
+
+local pool = PGConnectionPool.new(5, {
+  database  = 'mydb',
+  user      = 'postgres',
+  password  = 'secret',
+  reconnect = 1;
+})
+
+uv.timer():start(0, 1000, function()
+  local qry = pool:get()
+  if qry then qry:query('select 1', function(self, err, rs)
+    print(self, rs[1][1])
+  end) end
+end):unref()
+
+uv.timer():start(20000, function()
+  pool:close()
+end):unref()
+
+pool:on('connection::ready', function(pool, _, db)
+  print(db, 'connected')
+end)
+
+pool:on('connection::close', function(pool, _, db, err)
+  print(db, 'disconnected', err)
+end)
+
+uv.run()
